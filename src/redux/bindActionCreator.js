a3c696a20836565f782a7be39c3c4578c491b116
lym <954506993@qq.com>
2019-06-10 02:05:09 +0800

    修改 bindActionCreator

--- src/redux/bindActionCreator.js
+++ src/redux/bindActionCreator.js
@@ -1,6 +1,6 @@
 export default function bindActionCreator(actions, dispatch) {
-  if (typeof actions !== 'object') {
-    throw new Error('actions不是一个对象')
+  if (typeof actions !== 'object' && typeof actions !== 'function') {
+    throw new Error('actions需要是对象或者函数')
   }
   if (typeof dispatch !== 'function') {
     throw new Error('actions不是一个函数')
@@ -8,12 +8,16 @@ export default function bindActionCreator(actions, dispatch) {
 
   let res = {}
   if (actions !== null) {
-    Object.keys(actions).forEach(key => {
-      res[key] = function(...args) {
-        const action = actions[key]
-        dispatch(action(...args))
-      }
-    })
+    if (typeof actions === 'object') {
+      Object.keys(actions).forEach(key => {
+        res[key] = function(...args) {
+          const action = actions[key]
+          dispatch(action(...args))
+        }
+      })
+    } else {
+      res = actions(dispatch)
+    }
   }
   return res
 }
